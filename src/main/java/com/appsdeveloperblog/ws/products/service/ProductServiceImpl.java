4c84496ec62ac26e2fb57d7a3d57e77b43c24b0c
Jerry Lumontod <>
2024-02-07 12:10:48 +0800

    kafka synchronous

--- src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -1,13 +1,15 @@
 package com.appsdeveloperblog.ws.products.service;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
-import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
+
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -25,23 +27,32 @@ public class ProductServiceImpl implements ProductService {
 		String productId = UUID.randomUUID().toString();
 		
 		// TODO: Persist Product Details into database table before publishing an Event
-		
+
 		ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
-				productRestModel.getTitle(), productRestModel.getPrice(), 
+				productRestModel.getTitle(), productRestModel.getPrice(),
 				productRestModel.getQuantity());
-		
-		LOGGER.info("Before publishing a ProductCreatedEvent");
-		
-		SendResult<String, ProductCreatedEvent> result = 
-				kafkaTemplate.send("product-created-events-topic",productId, productCreatedEvent).get();
-		
-		LOGGER.info("Partition: " + result.getRecordMetadata().partition());
-		LOGGER.info("Topic: " + result.getRecordMetadata().topic());
-		LOGGER.info("Offset: " + result.getRecordMetadata().offset());
-		
+
+		CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+				kafkaTemplate.send("product-created-events-topic",productId, productCreatedEvent);
+
+		future.whenComplete((result, exception) -> {
+
+			if(exception != null) {
+				LOGGER.error("****** Failed to send message: " + exception.getMessage());
+			} else {
+				LOGGER.info("******* Message sent successfully: " + result.getRecordMetadata());
+			}
+
+		});
+
+		//this will become synchronous operation
+		//future.join();
+
 		LOGGER.info("***** Returning product id");
-		
+
 		return productId;
+
 	}
+
 
 }
